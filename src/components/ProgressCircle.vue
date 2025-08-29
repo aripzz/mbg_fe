03c1fae8a43db0fe95957adc55c4102eb68f9b14
aripzz <79347655+aripzz@users.noreply.github.com>
2025-08-29 09:45:43 +0700

    Update ProgressCircle.vue

--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="relative w-48 h-48">
-    <svg class="w-full h-full transform -rotate-90">
-      <!-- Track -->
-      <circle class="text-blue-300" stroke="currentColor" fill="transparent" stroke-width="18" cx="96" cy="96" r="79" />
-      <!-- Progress -->
-      <circle class="text-yellow-400 transition-all duration-700 ease-out" stroke="currentColor" fill="transparent"
-        stroke-width="18" stroke-linecap="round" :stroke-dasharray="circumference" :stroke-dashoffset="dashOffset"
-        cx="96" cy="96" r="84" />
-    </svg>
+  <div class="w-full relative" style="padding-bottom: 100%;">
+    <div class="absolute inset-0">
+      <svg class="w-full h-full transform -rotate-90" viewBox="0 0 192 192">
+        <circle class="text-white" stroke="currentColor" fill="currentColor" stroke-width="18" cx="96" cy="96" r="52" />
 
-    <!-- Percentage & Label -->
-    <div class="absolute bg-white mt-8 mr-8 ml-8 mb-9 rounded-full inset-0 flex flex-col items-center justify-center">
-      <p class="text-[32px] text-center font-bold text-[#307FF5]">
-        {{ percentage }}%
-      </p>
-      <span class="text-[10px] text-center leading-tight text-[#7D95C9]">
-        Diupdate <br /> {{ updatedAt }}
-      </span>
+
+        <circle class="text-blue-300" stroke="currentColor" fill="transparent" stroke-width="18" cx="96" cy="96"
+          r="79" />
+
+        <circle class="text-yellow-400 transition-all duration-700 ease-out" stroke="currentColor" fill="transparent"
+          stroke-width="18" stroke-linecap="round" :stroke-dasharray="circumference" :stroke-dashoffset="dashOffset"
+          cx="96" cy="96" r="84" />
+      </svg>
+
+      <div class="absolute inset-0 flex flex-col items-center justify-center">
+        <p class="text-[32px] text-center font-bold text-[#307FF5]">
+          {{ percentage }}%
+        </p>
+        <span class="text-[10px] text-center leading-tight text-[#7D95C9]">
+          Diupdate <br /> {{ updatedAt }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
